55334b74ab73aef720c8644a012cf0fca5eda791
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-03-28 00:39:07 +0545

    completed

--- downloader.py
+++ downloader.py
@@ -1,5 +1,6 @@
 import sys
 from pytube import YouTube as youtube
+import subprocess
 
 url = sys.argv[1]
 
@@ -11,11 +12,28 @@ else:
 #print("url= ", url)
 #print("format = ", media_type)
 
-print("Parsing video ...")
+print("Parsing video ;)  ...")
 yt = youtube(url)
+print(yt.title)
 if media_type == 'video':
-    stream = yt.streams.get_highest_resolution()
+#    stream = yt.streams.filter(res='1080p')[-1]
+#    if not stream:
+#        stream = yt.streams.filter(res='720p')[-1]
+#        if not stream:
+#            stream = yt.streams.first()
+
+    
+    stream = yt.streams.first()
 else:
     stream = yt.streams.get_audio_only()
 
+print("Downloading ...")
+stream.download()
 
+if media_type != 'video':
+    print("Converting ...")
+    filename = stream.default_filename.split(".mp")[0]+'.mp3'
+    subprocess.call(['ffmpeg','-i',stream.default_filename,filename])
+    subprocess.call(['rm',stream.default_filename])
+
+print("*********Completed***********")
